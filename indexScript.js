1ef41e1e5e038a6cfa336df8545cede1a80f38d8
ananyaprabhakarm <ananyaprabhakarm@gmail.com>
2023-10-17 20:32:25 +0530

    added car and few more functionalities

--- indexScript.js
+++ indexScript.js
@@ -3,7 +3,11 @@ let high = document.getElementById('highScore');
 let restartOption = document.getElementById('reStart');
 let gameArea = document.getElementById("gameplay");
 let scoring = false;
+let player = {};
+let car = document.getElementById('car');
+let scorestart = document.getElementById('scoring');
 let gameButton = document.getElementById('startButton');
+player.start = false;
 let sc = 0;
 
 document.addEventListener("keydown", scoreStart);
@@ -12,7 +16,11 @@ document.addEventListener("keyup", scoreStop);
 function scoreStart(event) {
   if (event.key === "ArrowUp") {
     scoring = true;
-    console.log("abc");
+    player.start = true;
+    player.y = (player.y - 5);
+    car.style.top = player.y + "px";
+    console.log(player.y);
+    
     scoringLoop();
   }
 }
@@ -20,6 +28,8 @@ function scoreStart(event) {
 function scoreStop(event) {
   if (event.key === "ArrowUp") {
     scoring = false;
+    player.start = false;
+    
   }
 }
 
@@ -35,3 +45,18 @@ function updateScore(scoreValue) {
   score.textContent = scoreValue.toString().padStart(3, "0");
 }
   
+// gameButton.addEventListener("click", start);
+
+if(player.start === false){
+  scorestart.style.display = 'none';
+}
+
+gameButton.addEventListener('click', function () {
+  player.start = true;
+  gameButton.style.display = 'none';
+  scorestart.style.display = 'flex';
+});
+   
+player.x = car.offsetLeft;
+player.y = car.offsetTop;
+
